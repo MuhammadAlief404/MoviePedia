47b261288ba1974b10212f877df7c2ef158e6c69
Muhammad Alief <malief.dev@gmail.com>
2022-06-20 14:00:32 +0800

    add encryption to database

--- core/src/main/java/com/quantumhiggs/moviepedia/core/di/DatabaseModule.kt
+++ core/src/main/java/com/quantumhiggs/moviepedia/core/di/DatabaseModule.kt
@@ -10,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -18,10 +20,16 @@ class DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): MoviePediaDatabase = Room.databaseBuilder(
-        context,
-        MoviePediaDatabase::class.java, "MoviePedia.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): MoviePediaDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("moviePedia".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MoviePediaDatabase::class.java, "MoviePedia.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideMoviesDao(database: MoviePediaDatabase): MoviesDao = database.moviesDao()
